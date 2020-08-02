816915703365ef3a334f2a723cc36bccf48706b8
osha7 <ogroetz@gmail.com>
2020-08-02 16:37:07 -0600

    add divs: header, item-container

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("testing...")
+
+
+const main = document.querySelector('#main')
+
+const divAddItem = document.createElement('div')
+    divAddItem.id = 'add-new-item'
+    main.appendChild(divAddItem)
+
+const divAddHeader = document.createElement('div')
+    divAddHeader.id = 'header-div'
+    main.appendChild(divAddHeader)
+const addHeader = document.createElement('h1')
+    addHeader.innerText = 'Ware'
+    divAddHeader.appendChild(addHeader)
+
+const divItemCard = document.createElement('div')
+    divItemCard.id = 'item-card-container'
+    main.appendChild(divItemCard)
+
+// function addItemDiv(e) {
+//     divAddItem.innerHTML = `
+//     <inn
+//     `
+// }
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    fetch("http://localhost:3000/items")
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(json) {
+            addItemCard(json);
+        })
+})
+
+function addItemCard(item) {
+    let cardContainer = document.getElementById('item-card-container')
+    cardContainer.innerHTML += `
+        <div class="card">
+        <img src=${item['img_source']} class="item-image" />
+        <h2 id=${item['id']}></h2>
+        <p>Price: $${item['price']}</p>
+        <p>Availability: ${item['item_count']}</p>
+        <p>${item['description']}</p>
+        <button class="add-to-cart">Add Item To Cart</button>
+        </div>
+    `
+}
+
+
+
