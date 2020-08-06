dcbbf31afb595668b2a54d5fdfb4d6e783e79705
osha7 <ogroetz@gmail.com>
2020-08-06 12:09:01 -0600

    destructuring + toggle btwn shop/cart

--- index.js
+++ index.js
@@ -5,28 +5,59 @@ const cartUrl = "http://localhost:3000/create-or-return-cart"
 
 const main = document.querySelector('#main')
     main.innerHTML += `
-    <div id='shopping-container'>
-        <div id="header-div">
-            <h1>Ware</h1>
-        </div>
-        <div class="shopping-cart-counter">
-            <div class='shopping-cart-image'>
-                <i class='fas fa-shopping-bag' style='font-size:36px'></i>
+        <div id="shopping-container">
+            <div id="header-div">
+                <h1>Ware</h1>
             </div>
-            <div class='cart-counter'></div>
+            <div class="shopping-cart-counter">
+                <div class="shopping-cart-image">
+                    <i class="fas fa-shopping-bag" style='font-size:36px'></i>
+                </div>
+                <div class="cart-counter"></div>
+            </div>
+            <div id="add-new-item"></div>
+            <div id="item-card-container"></div>
         </div>
-        <div id="add-new-item"></div>
-        <div id="item-card-container"></div>
-    </div>
+        <div id="cart-container">
+            <h1>Cart</h1>
+            <div id="container" class="cart-card-container">
+                <div id="card" class="cart-card">
+                    <div class="cart-items">
+                        <table id="item-table" style="width:100%">
+                            <tr style="font-size: 22px; text-decoration: underline;">
+                                <th>Item Name</th>
+                                <th>#</th>
+                                <th>Price</th>
+                                <th></th>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div id="button" class="shop-return">
+            <button class="return-to-shop">Shop</button>
+            </div>
+        </div>
     `
+
+
 let itemCardContainerDiv = document.getElementById('item-card-container')
 let shoppingCartCounter = document.getElementsByClassName('cart-counter')[0] 
 let addToCartButton = document.getElementsByClassName('add-to-cart')[0]
+
+let showCart = false
+// show this if showCart is false
+let shoppingContainerDiv = document.getElementById('shopping-container')
+// show this if showCart is true
+let cartContainerDiv = document.getElementById('cart-container')
+
 let shoppingCartId = 0
+
 // let seePromiseFromFetch = fetch(itemUrl)
 
 document.addEventListener('DOMContentLoaded', fetchAllItems)
 document.addEventListener('DOMContentLoaded', fetchCurrentCart)
+
     // debugger
 
 function fetchAllItems() {
@@ -44,19 +75,28 @@ function fetchAllItems() {
         .catch(() => alert("Can’t access " + itemUrl + " response. Blocked by browser?"))
 }
 
-function addItemCard(item) {
+// destructuring ES6:
+
+// let itemObj = { 
+//     name: "Jeff",
+// }
+
+// let {name} = itemObj
+
+// using destructuring ES6:
+
+function addItemCard({id, img_source, name, price, item_count, description, cart_id}) {
     return `
-        <div id=item-${item.id}-card class="card">
-            <img src=${item.img_source} class="item-image"/>
-            <h2>${item.name}</h2>
-            <p>Price: $${item.price} - Availability: ${item['item_count']} <br>
-                ${item.description}</p>
-            <button class="add-to-cart" data-item=${item.id}>Add Item To Cart</button>
-            <p hidden id="item-cart-id">${item.cart_id}</p>
+        <div id=item-${id}-card class="card">
+            <img src=${img_source} class="item-image"/>
+            <h2>${name}</h2>
+            <p>Price: $${price} - Availability: ${item_count} <br>
+                ${description}</p>
+            <button class="add-to-cart" data-item=${id}>Add Item To Cart</button>
+            <p hidden id="item-cart-id">${cart_id}</p>
         </div>
     `
 }
-
 
 function fetchCurrentCart () {
     fetch(cartUrl)
@@ -69,8 +109,6 @@ function fetchCurrentCart () {
         })
         .catch(() => alert("Can’t access " + cartUrl + " response."))
 }
-
-
 
 document.addEventListener('click', function(e) {
     e.preventDefault()
@@ -116,11 +154,31 @@ document.addEventListener('click', function(e) {
     }
 })
 
+// document.addEventListener('click', function(e) {
+//     if (e.target.className === "fas fa-shopping-bag") {
+//     let shoppingArea = document.getElementById('shopping-container')
+//     shoppingArea.innerHTML = " "
+//     showCurrentCart()
+//     }
+// })
+
 document.addEventListener('click', function(e) {
     if (e.target.className === "fas fa-shopping-bag") {
-    let shoppingArea = document.getElementById('shopping-container')
-    shoppingArea.innerHTML = " "
-    showCurrentCart()
+        showCart = !showCart
+        if (showCart) {
+            shoppingContainerDiv.style.display = "none";
+            showCurrentCart()
+            cartContainerDiv.style.display = "block";
+
+            // setTimeout(function() { 
+            //     showCurrentCart()
+            //     cartContainerDiv.style.display = "block"; 
+            // }, 0)
+            
+        // } else {
+        //     cartContainerDiv.style.display = "block";
+        //     shoppingContainerDiv.style.display = "none";
+        }
     }
 })
 
@@ -133,27 +191,6 @@ const showCurrentCart = function() {
 
 function renderCartPage(cart) {
     // debugger
-    main.innerHTML += `
-    <div id=cart-${cart.id} class="cart-container">
-    <h1>Cart</h1>
-        <div id="container" class="cart-card-container">
-            <div id="card" class="cart-card">
-                <div class="cart-items">
-                    <table id="item-table" style="width:100%">
-                        <tr style="font-size: 22px; text-decoration: underline;">
-                            <th>Item Name</th>
-                            <th>#</th>
-                            <th>Price</th>
-                            <th></th>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-        <div id="button" class="shop-return">
-        <button class="return-to-shop">Shop</button>
-    </div>
-    `
     let itemsTable = document.getElementById('item-table')
     let cartItemsArray = cart.items
     cartItemsArray.map( item => {
@@ -195,19 +232,22 @@ document.addEventListener('click', function(e) {
             // })
             .then(e.target.parentElement.parentElement.remove())
 
-            .catch(err => {
-                console.log(`Error: ${err}`)
-            })
+            .catch(() => alert("Can’t access " + removeItemPath + " response."))
     }
 })
 
 document.addEventListener('click', function(e) {
     if (e.target.className === "return-to-shop") {
         // debugger
-        let cartArea = document.getElementsByClassName('cart-container')[0]
-        cartArea.innerHTML = " "
-        
-        fetchAllItems()
+        showCart = !showCart
+        if (showCart != true) {
+            shoppingContainerDiv.style.display = "block";
+            // let cartContainerDiv = document.getElementsByClassName('cart-container')[0]
+            cartContainerDiv.style.display = "none";
+            
+        // } else {
+        //     shoppingContainerDiv.style.display = "block";
+        //     cartContainerDiv.style.display = "none";
+        }
     }
 })
-
