049b4892c94236e09b60c522d7d3c736ad51031c
osha7 <ogroetz@gmail.com>
2020-08-12 08:54:42 -0600

    clean up itemsadapter

--- src/itemsAdapter.js
+++ src/itemsAdapter.js
@@ -11,7 +11,6 @@ class itemsAdapter {
         this.addToCartButton = document.getElementsByClassName('add-to-cart')[0]
         this.shoppingContainerDiv = document.getElementById('shopping-container')
 
-        // this.document.addEventListener('click', this.addToCart)
         this.items = []
     }
 
@@ -37,64 +36,5 @@ class itemsAdapter {
             .catch(() => alert("Can’t access " + this.baseURL + " response. Blocked by browser?"))
     }
 
-    // addItemCard({id, img_source, name, price, item_count, description, cart_id}) {
-    //     return `
-    //         <div id=item-${id}-card class="card">
-    //             <img src=${img_source} class="item-image"/>
-    //             <h2>${name}</h2>
-    //             <p>Price: $${price} - Availability: ${item_count} <br>
-    //                 ${description}</p>
-    //             <button class="add-to-cart" data-item=${id}>Add Item To Cart</button>
-    //             <p hidden id="item-cart-id">${cart_id}</p>
-    //         </div>
-    //     `
-    // }
 
-    // addToCart(e) {
-    //     e.preventDefault()
-    //     if (e.target.className === "add-to-cart") {
-    //         newArray = cartArrayOfItems.map (item => item.id)
-    //         if (newArray.includes(parseInt(e.target.dataset.item))) {
-    //             alert("You already have this item in your cart!")
-    //         } else {
-    //         // debugger
-    //         e.target.disabled = true
-    //         e.target.innerText = "Item Added To Cart"
-    //         let itemPath = `http://localhost:3000/items/${e.target.dataset.item}`
-    //         // debugger
-    //         const bodyData = {item: {
-    //             cart_id: shoppingCartId
-    //             }
-    //           }
-    //             fetch(itemPath, {
-    //                 method: 'PATCH',
-    //                 headers: {
-    //                     "Content-Type": "application/json",
-    //                     "Accept": "application/json"
-    //                   },
-    //                 body: JSON.stringify(bodyData)
-    //             })
-    //             .then(response => response.json())
-    //     // optimistically change the shopping bag tally 
-    //             .then(function(e) {
-    //                 let i = document.getElementsByClassName('card').length
-    //                 let counterTally = parseInt(shoppingCartCounter.innerText)
-    //                 // if e.cart_id != true
-    //                 // debugger
-    //                 if (counterTally < i) {
-    //                     counterTally++
-    //                     shoppingCartCounter.innerText = `${counterTally}`
-    //                 } else {
-    //                     alert("That was all of the items available for sale!")
-    //                 }
-    //             })
-    //             // .catch(err => {
-    //             //     console.log(`Error: ${err}`)
-    //             // })
-    //             .catch(() => alert("Can’t access " + itemPath + " response."))
-    //         }
-    //     }
-    // }
-
-    
 }
